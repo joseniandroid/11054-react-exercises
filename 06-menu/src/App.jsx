84b31d775c94b2c729666612ed1653d7995400ef
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-04 11:49:25 +0800

    render the individual menu items

--- 06-menu/src/App.jsx
+++ 06-menu/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Categories from "./Categories";
+import Menu from "./Menu";
 import items from "./data";
 
 const allCategories = ["all", ...new Set(items.map((item) => item.category))];
@@ -16,7 +17,7 @@ function App() {
           <div className="underline"></div>
         </div>
         <Categories categories={categories} />
-        <div></div>
+       <Menu items={menuItems} />
       </section>
     </main>
   );
