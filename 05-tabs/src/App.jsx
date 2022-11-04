b0165ae33c0cdc4990ba34db8acaaeaadefc8ede
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-04 11:06:03 +0800

    implement the base app structure and the Loading component

--- 05-tabs/src/App.jsx
+++ 05-tabs/src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import Loading from "./Loading";
+
 function App() {
-  return <h3>Tabs Project Setup</h3>;
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* TODO: Loop through all the jobs fetch from the API server */}
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
+  );
 }
 
 export default App;
