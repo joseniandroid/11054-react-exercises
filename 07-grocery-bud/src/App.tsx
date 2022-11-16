29f955849573d687055241e4e774fe361e157bc6
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-16 12:16:05 +0800

    add feature to remove item from the basket

--- 07-grocery-bud/src/App.tsx
+++ 07-grocery-bud/src/App.tsx
@@ -87,6 +87,21 @@ function App() {
     setEditId(id);
   };
 
+  const removeItem = (id: string) => {
+    const itemToDelete = basket.find((item) => item.id === id);
+    if (!itemToDelete) {
+      return;
+    }
+
+    const updatedBasket = basket.filter((item) => item.id !== id);
+    setBasket(updatedBasket);
+    showAlert({
+      show: true,
+      message: `'${itemToDelete.title}' removed from the basket`,
+      type: 'success',
+    });
+  };
+
   return (
     <section className='section-center'>
       <form action='' className='grocery-form' onSubmit={handleSubmit}>
@@ -109,7 +124,7 @@ function App() {
 
       {basket.length > 0 && (
         <div className='grocery-container'>
-          <Basket items={basket} editItem={editItem} />
+          <Basket items={basket} editItem={editItem} removeItem={removeItem} />
           <button className='clear-btn' onClick={clearBasket}>
             clear items
           </button>
