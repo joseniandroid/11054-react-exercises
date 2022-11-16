d0dc4da868b82feada01819a04fe16ebd5b4cb0a
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-16 11:11:34 +0800

    bind itemName to input text

--- 07-grocery-bud/src/App.tsx
+++ 07-grocery-bud/src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+
 function App() {
+  const [itemName, setItemName] = useState('eggs');
+
   return (
     <section className='section-center'>
       <form action='' className='grocery-form'>
         <h3>grocery bud</h3>
         <div className='form-control'>
-          <input type='text' className='grocery' placeholder='e.g. eggs' />
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+          />
+          <h2>{itemName}</h2>
           <button type='submit' className='submit-btn'>
             submit
           </button>
