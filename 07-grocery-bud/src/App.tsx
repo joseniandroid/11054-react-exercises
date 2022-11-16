144cec0cc7e011ec6de90247972b978d0ed7533a
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-16 11:06:27 +0800

    setup the base app structure

--- 07-grocery-bud/src/App.tsx
+++ 07-grocery-bud/src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <h2>Grocery Bud Project Setup</h2>;
+  return (
+    <section className='section-center'>
+      <form action='' className='grocery-form'>
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input type='text' className='grocery' placeholder='e.g. eggs' />
+          <button type='submit' className='submit-btn'>
+            submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default App;
