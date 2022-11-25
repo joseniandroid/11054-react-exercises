678f5b0178f1a78682731e3be4e940ea358e411c
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-25 11:41:31 +0800

    demo on using react context

--- 09-useContext-basics/src/WithUseContext/Parent.tsx
+++ 09-useContext-basics/src/WithUseContext/Parent.tsx
@@ -1,13 +1,17 @@
+import { createContext, useState } from 'react';
+
 import { Child1 } from './children';
-import { useState } from 'react';
+
+export const CountContext = createContext(0);
 
 export const Parent = () => {
   const [count, setCount] = useState(0);
+
   return (
-    <>
+    <CountContext.Provider value={count}>
       <Child1 />
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Add</button>
-    </>
+    </CountContext.Provider>
   );
 };
--- 09-useContext-basics/src/WithUseContext/children/Child1.tsx
+++ 09-useContext-basics/src/WithUseContext/children/Child1.tsx
@@ -1,10 +1,15 @@
 import { Child2 } from './Child2';
+import { CountContext } from '../Parent';
 import styles from './Child.module.css';
+import { useContext } from 'react';
 
 export const Child1 = () => {
+  const count = useContext(CountContext);
+
   return (
     <div className={styles.childBox1}>
       <Child2 />
+      <h1 className={styles.boxHeader}>{count}</h1>
     </div>
   );
 };
