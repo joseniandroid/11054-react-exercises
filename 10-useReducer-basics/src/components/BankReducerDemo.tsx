4cbd934780eaebb7ee742b3815d61e6c5a2dc304
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-28 11:45:03 +0800

    simplify the deposit and withdraw actions

--- 10-useReducer-basics/src/components/BankReducerDemo.tsx
+++ 10-useReducer-basics/src/components/BankReducerDemo.tsx
@@ -1,29 +1,9 @@
-import {
-  BankAction,
-  BankActionType,
-  bankReducer,
-  initialState,
-} from '../reducers/bank';
+import { bankReducer, deposit, initialState, withdraw } from '../reducers/bank';
 import { useReducer, useState } from 'react';
 
 export const BankReducerDemo = () => {
   const [amount, setAmount] = useState(0);
   const [state, dispatch] = useReducer(bankReducer, initialState);
-
-  const handleDeposit = (amount: number) => {
-    const depositAction: BankAction = {
-      type: BankActionType.Deposit,
-      payload: amount,
-    };
-    dispatch(depositAction);
-  };
-
-  const handleWithdrawal = (amount: number) => {
-    dispatch({
-      type: BankActionType.Withdraw,
-      payload: amount,
-    });
-  };
 
   const handleSetAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
     const amountValue = parseInt(e.target.value);
@@ -36,9 +16,9 @@ export const BankReducerDemo = () => {
       <input type='number' value={amount} onChange={handleSetAmount} />
 
       <div style={{ marginTop: '1em' }}>
-        <button onClick={() => handleDeposit(amount)}>Deposit</button>
+        <button onClick={() => dispatch(deposit(amount))}>Deposit</button>
         <span style={{ marginLeft: '1em' }}></span>
-        <button onClick={() => handleWithdrawal(amount)}>Withdraw</button>
+        <button onClick={() => dispatch(withdraw(amount))}>Withdraw</button>
       </div>
     </>
   );
--- 10-useReducer-basics/src/reducers/bank/actions.ts
+++ 10-useReducer-basics/src/reducers/bank/actions.ts
@@ -7,3 +7,13 @@ export type BankAction = {
   type: BankActionType;
   payload: number;
 };
+
+export const deposit = (amount: number): BankAction => ({
+  type: BankActionType.Deposit,
+  payload: amount,
+});
+
+export const withdraw = (amount: number): BankAction => ({
+  type: BankActionType.Withdraw,
+  payload: amount,
+});
