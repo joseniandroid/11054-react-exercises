9107cd11617bd2678e7839521dd20bd58c9ce094
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-28 11:56:05 +0800

    demo on handling 3 states in a component

--- 10-useReducer-basics/src/components/TriStateReducerDemo.tsx
+++ 10-useReducer-basics/src/components/TriStateReducerDemo.tsx
@@ -1,3 +1,28 @@
+import { useState } from 'react';
+
 export const TriStateReducerDemo = () => {
-  return <div>TriStateReducerDemo</div>;
+  const [inputText, setInputText] = useState('');
+  const [count, setCount] = useState(0);
+  const [isDefaultColor, setIsDefaultColor] = useState(true);
+
+  return (
+    <main style={{ color: isDefaultColor ? 'black' : 'red' }}>
+      <h2>TriState Reducer Demo</h2>
+      <input
+        type='text'
+        placeholder='Enter text'
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <h3>{count}</h3>
+      <h3>{inputText}</h3>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setIsDefaultColor(!isDefaultColor)}>
+          Toggle Color
+        </button>
+      </div>
+    </main>
+  );
 };
